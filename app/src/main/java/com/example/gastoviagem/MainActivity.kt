8564cfcc6adf35723a1445a4ed4a7d55476adbbb
Thooor7 <heitor.dev7@gmail.com>
2024-02-07 21:06:58 -0300

    first commit

---
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.gastoviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.gastoviagem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.buttonCalcular?.setOnClickListener(this)
+    }
+
+    private fun captureValues() {
+        var distancia = binding?.etDistance?.text.toString()
+        var preco = binding?.etPreco?.text.toString()
+        var autonomia = binding?.etAutonomia?.text.toString()
+
+        if (distancia.isNotEmpty() && preco.isNotEmpty() && autonomia.isNotEmpty()) {
+            try {
+                val distance = distancia.toFloat()
+                val price = preco.toFloat()
+                val autonomy = autonomia.toFloat()
+                calculateTotal(distance, price, autonomy)
+            } catch (e: NumberFormatException) {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.format_invalid),
+                    Toast.LENGTH_LONG
+                ).show()
+                binding?.tvTotal?.text = "R$ 0.00"
+            }
+        } else {
+            Toast.makeText(
+                this@MainActivity,
+                getString(R.string.complete_required_field),
+                Toast.LENGTH_LONG
+            ).show()
+            binding?.tvTotal?.text = "R$ 0.00"
+        }
+    }
+
+    private fun calculateTotal(distance: Float, preco: Float, autonomia: Float) {
+
+        var total = distance / autonomia * preco
+        val totalFormated = String.format("%.2f", total)
+        binding?.tvTotal?.text = "R$ ${totalFormated}"
+    }
+
+    override fun onClick(p0: View?) {
+        captureValues()
+    }
+}
